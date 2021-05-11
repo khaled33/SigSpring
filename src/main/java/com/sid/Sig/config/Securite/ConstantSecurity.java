e62f9205f8a3d450228540bf1cb7c18f85a7bd2d
khaled hizawi <khaledhizawi@gmail.com>
2021-05-11 07:07:05 +0100

    Authentication  module

---
+++ src/main/java/com/sid/Sig/config/Securite/ConstantSecurity.java
@@ -0,0 +1,9 @@
+package com.sid.Sig.config.Securite;
+
+public class ConstantSecurity {
+    public static final String SECRET="test";
+    public static final String TOKEN_PREFIX="Bearer ";
+    public static final String HEADER_STRING="Authorization";
+    public static final long EXPIRATION_TIME=9600000; // 1H
+
+}
---
+++ src/main/java/com/sid/Sig/config/Securite/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.sid.Sig.config.Securite;
+
+import com.sid.Sig.UserDetails.Service.ImpUserDetailsService;
+import com.sid.Sig.config.error.JWTAccessDeniedHandler;
+import com.sid.Sig.config.error.JWTAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/SendEmail",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+            // other public endpoints of your API may be appended to this array
+    };
+    @Autowired
+    private ImpUserDetailsService userDetailsService;
+    @Autowired
+    private JWTAccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    private JWTAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(bCryptPasswordEncoder);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+       // http.formLogin();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/actuator/**","/login/**","/Register/**","/ForgotPasswor/**","/ResetPassord/**","/getUserByEmail/**").permitAll();
+        http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST,"/tasks/**").hasAuthority("ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.exceptionHandling().accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(authenticationEntryPoint);
+        http.addFilter(new JWTAauthentficationFilter(authenticationManager()));
+        http.addFilterBefore(new JWTAuthorrizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+
+        return source;
+    }
+
+ 
+}
